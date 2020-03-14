5cbf3c28ddb9f30d18e68e4ddbfcaff58e145a00
600610721 <bang_dekdee@hotmail.co.th>
2020-03-15 01:34:47 +0700

    add sql

---
+++ sql/init.sql
@@ -0,0 +1,14 @@
+create table if not exists users.User
+(
+	id int auto_increment
+		primary key,
+	studentId varchar(256) null,
+	firstNameTH varchar(256) null,
+	firstNameEN varchar(256) null,
+	lastNameTH varchar(256) null,
+	lastNameEN varchar(256) null,
+	organizationNameTH varchar(256) null,
+	organizationNameEN varchar(256) null,
+	constraint user_studentId_uindex
+		unique (studentId)
+);
