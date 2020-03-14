ecc5913265bdb88d7a7e3bf84bf0214ca5759dea
600610721 <bang_dekdee@hotmail.co.th>
2020-03-15 00:37:12 +0700

    add models

---
+++ src/models/user.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  studentId: string;
+
+  @property({
+    type: 'string',
+  })
+  firstNameTH?: string;
+
+  @property({
+    type: 'string',
+  })
+  firstNameEN?: string;
+
+  @property({
+    type: 'string',
+  })
+  lastNameTH?: string;
+
+  @property({
+    type: 'string',
+  })
+  lastNameEN?: string;
+
+  @property({
+    type: 'string',
+  })
+  organizationNameTH?: string;
+
+  @property({
+    type: 'string',
+  })
+  organizationNameEN?: string;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
