a4d82a95604eb3f963edf34145180586cbb50c79
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-03-29 19:33:46 -0700

    Refactoring code form

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (require.main === module) {
       },
     },
   };
-  application.main(config).catch(err => {
+  application.main(config).catch((err) => {
     console.error('Cannot start the application.', err);
     process.exit(1);
   });
